a9ebb457b05bbe9fdda93038784d17f1fd5939b3
wolverrrr <ryan.j.wolver@gmail.com>
2026-04-06 22:23:16 -0700

    General updates to the website, including adding a banner, fixing the title tag, and updating the hover effects for the social media icons.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleTheme() {
   document.body.classList.toggle('dark');
-  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light')
+  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 }
