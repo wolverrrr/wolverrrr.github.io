d7e265002951e6639774b3cebb192e9a70c96c1f
wolverrrr <ryan.j.wolver@gmail.com>
2026-04-07 11:18:02 -0700

    Added menu bars, dropdown script, and general updates to all files

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleTheme() {
   document.body.classList.toggle('dark');
   localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 }
+
+function dropMenu() {
+  
+}
