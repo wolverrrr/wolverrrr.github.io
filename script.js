e5e6d8112d14e86ed72b766b3b15fa8e55fc3911
wolverrrr <ryan.j.wolver@gmail.com>
2026-08-19 16:31:49 -0700

    Updated nav bar, still in progress Lowered social icons Reorganized CSS, added transitions to links and brand

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+const nav = document.getElementById('siteNav');
+const threshold = 60;
+let ticking = false;
+
+function updateNav() {
+  if (window.scrollY > threshold) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+  ticking = false;
+}
+
+window.addEventListener('scroll', () => {
+  if (!ticking) {
+    window.requestAnimationFrame(updateNav);
+    ticking = true;
+}
+});
+
+
 function toggleTheme() {
   document.body.classList.toggle('dark');
   localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 }
 
-function dropMenu() {
-  
-}
+
